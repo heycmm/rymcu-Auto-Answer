b42eb7abce0d7761a76cfa99c1227e5850987027
czx <heycmm.cn@gmail.com>
2022-06-29 20:02:03 +0800

    :tada::tada::tada::tada:

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ import json
 
 import requests
 from setting import headers, login_url, today_url, show_answer_url, answer_url, account, pw
+from requests.adapters import HTTPAdapter
 
+r = requests.Session()
+# 超时重试3次
+r.mount('http://', HTTPAdapter(max_retries=3))
+r.mount('https://', HTTPAdapter(max_retries=3))
 
 def auto_answer():
     login_data = {"account": account, "password": pw}
-    user = requests.post(login_url, headers=headers, data=json.dumps(login_data)).json()
+    user = r.post(login_url, headers=headers, data=json.dumps(login_data)).json()
     if user['code'] == 0:
         headers['authorization'] = user['data']['user']['token']
-        today = requests.get(today_url, headers=headers).json()
+        today = r.get(today_url, headers=headers).json()
         if today['code'] == 0:
             answer_id = today['data']['answerRecords'][0]['id']
             print('今日试题：：%s' % today['data']['answerRecords'][0])
@@ -18,7 +23,7 @@ def auto_answer():
                 correct_answer = answer['data']['respData']
                 print('正确答案：%s' % correct_answer)
                 answer_content = {'idSubjectQuestion': answer_id, 'answer': correct_answer}
-                print(requests.post(answer_url, headers=headers, data=json.dumps(answer_content)).json())
+                print(r.post(answer_url, headers=headers, data=json.dumps(answer_content)).json())
 
 
 if __name__ == '__main__':
