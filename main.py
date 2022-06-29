b58da2bde2e7c5b24ed69b6645bce081e5acb68c
czx <heycmm.cn@gmail.com>
2022-06-29 20:03:07 +0800

    :tada::tada::tada::tada:

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ r = requests.Session()
 r.mount('http://', HTTPAdapter(max_retries=3))
 r.mount('https://', HTTPAdapter(max_retries=3))
 
+
 def auto_answer():
     login_data = {"account": account, "password": pw}
     user = r.post(login_url, headers=headers, data=json.dumps(login_data)).json()
@@ -18,7 +19,7 @@ def auto_answer():
         if today['code'] == 0:
             answer_id = today['data']['answerRecords'][0]['id']
             print('今日试题：：%s' % today['data']['answerRecords'][0])
-            answer = requests.get(show_answer_url % answer_id).json()
+            answer = r.get(show_answer_url % answer_id).json()
             if answer['success']:
                 correct_answer = answer['data']['respData']
                 print('正确答案：%s' % correct_answer)
